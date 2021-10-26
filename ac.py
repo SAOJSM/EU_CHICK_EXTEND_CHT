6a069d7af2cd2d973ea047ce270ff51234848929
SAOJSM <46414240+SAOJSM@users.noreply.github.com>
2021-10-26 18:11:19 +0800

    Update ac.py

--- ac.py
+++ ac.py
@@ -305,8 +305,8 @@ def telegram():
 
         if __name__ == "__main__":
             if not USERNAME or not PASSWORD:
-            log("[EUserv] 你沒有添加任何賬戶")
-            exit(1)
+                log("[EUserv] 你沒有添加任何賬戶")
+                exit(1)
     user_list = USERNAME.strip().split()
     passwd_list = PASSWORD.strip().split()
     if len(user_list) != len(passwd_list):
