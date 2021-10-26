9c9c074769bd9f2e028027533a624cd969d13d64
SAOJSM <46414240+SAOJSM@users.noreply.github.com>
2021-10-26 16:45:11 +0800

    Create ac.py

---
+++ ac.py
@@ -0,0 +1,342 @@
+#! /usr/bin/env python3
+
+import re
+import json
+import time
+import base64
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+TG_API_HOST = 'api.telegram.org'
+USERID = "johndoe"
+APIKEY = "deJhWBaqgd6QDN4BqJGf"
+
+# Magic internet access
+#PROXIES = {"http": "http://127.0.0.1:10808", "https": "http://127.0.0.1:10808"}
+
+
+# Maximum number of login retry
+LOGIN_MAX_RETRY_COUNT = 5
+
+
+# options: True or False
+CHECK_CAPTCHA_SOLVER_USAGE = True
+
+
+user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/94.0.4606.61 Safari/537.36 "
+)
+desp = ""  # 空值
+
+
+def log(info: str):
+    print(info)
+    global desp
+    desp = desp + info + "\n\n"
+
+
+def login_retry(*args, **kwargs):
+    def wrapper(func):
+        def inner(username, password):
+            ret, ret_session = func(username, password)
+            max_retry = kwargs.get("max_retry")
+            # default retry 3 times
+            if not max_retry:
+                max_retry = 3
+            number = 0
+            if ret == "-1":
+                while number < max_retry:
+                    number += 1
+                    if number > 1:
+                        log("[EUserv] Login tried the {}th time".format(number))
+                    sess_id, session = func(username, password)
+                    if sess_id != "-1":
+                        return sess_id, session
+                    else:
+                        if number == max_retry:
+                            return sess_id, session
+            else:
+                return ret, ret_session
+
+        return inner
+
+    return wrapper
+
+
+def captcha_solver(captcha_image_url: str, session: requests.session) -> dict:
+    """
+    TrueCaptcha API doc: https://apitruecaptcha.org/api
+    Free to use 100 requests per day.
+    """
+    response = session.get(captcha_image_url)
+    encoded_string = base64.b64encode(response.content)
+    url = "https://api.apitruecaptcha.org/one/gettext"
+
+    data = {
+        "userid": USERID,
+        "apikey": APIKEY,
+        "case": "mixed",
+        "mode": "human",
+        "data": str(encoded_string)[2:-1],
+    }
+    r = requests.post(url=url, json=data)
+    j = json.loads(r.text)
+    return j
+
+
+def handle_captcha_solved_result(solved: dict) -> str:
+    """Since CAPTCHA sometimes appears as a very simple binary arithmetic expression.
+    But since recognition sometimes doesn't show the result of the calculation directly,
+    that's what this function is for.
+    """
+    if "result" in solved:
+        solved_text = solved["result"]
+        if "RESULT  IS" in solved_text:
+            log("[Captcha Solver] You are using the demo apikey.")
+            print("There is no guarantee that demo apikey will work in the future!")
+            # because using demo apikey
+            text = re.findall(r"RESULT  IS . (.*) .", solved_text)[0]
+        else:
+            # using your own apikey
+            log("[Captcha Solver] You are using your own apikey.")
+            text = solved_text
+        operators = ["X", "x", "+", "-"]
+        if any(x in text for x in operators):
+            for operator in operators:
+                operator_pos = text.find(operator)
+                if operator == "x" or operator == "X":
+                    operator = "*"
+                if operator_pos != -1:
+                    left_part = text[:operator_pos]
+                    right_part = text[operator_pos + 1 :]
+                    if left_part.isdigit() and right_part.isdigit():
+                        return eval(
+                            "{left} {operator} {right}".format(
+                                left=left_part, operator=operator, right=right_part
+                            )
+                        )
+                    else:
+                        # Because these symbols("X", "x", "+", "-") do not appear at the same time,
+                        # it just contains an arithmetic symbol.
+                        return text
+        else:
+            return text
+    else:
+        print(solved)
+        raise KeyError("Failed to find parsed results.")
+
+
+def get_captcha_solver_usage() -> dict:
+    url = "https://api.apitruecaptcha.org/one/getusage"
+
+    params = {
+        "username": USERID,
+        "apikey": APIKEY,
+    }
+    r = requests.get(url=url, params=params)
+    j = json.loads(r.text)
+    return j
+
+
+@login_retry(max_retry=LOGIN_MAX_RETRY_COUNT)
+def login(username: str, password: str) -> (str, requests.session):
+    headers = {"user-agent": user_agent, "origin": "https://www.euserv.com"}
+    url = "https://support.euserv.com/index.iphp"
+    captcha_image_url = "https://support.euserv.com/securimage_show.php"
+    session = requests.Session()
+
+    sess = session.get(url, headers=headers)
+    sess_id = re.findall("PHPSESSID=(\\w{10,100});", str(sess.headers))[0]
+    # visit png
+    logo_png_url = "https://support.euserv.com/pic/logo_small.png"
+    session.get(logo_png_url, headers=headers)
+
+    login_data = {
+        "email": username,
+        "password": password,
+        "form_selected_language": "en",
+        "Submit": "Login",
+        "subaction": "login",
+        "sess_id": sess_id,
+    }
+    f = session.post(url, headers=headers, data=login_data)
+    f.raise_for_status()
+
+    if (
+        f.text.find("Hello") == -1
+        and f.text.find("Confirm or change your customer data here") == -1
+    ):
+        if (
+            f.text.find(
+                "To finish the login process please solve the following captcha."
+            )
+            == -1
+        ):
+            return "-1", session
+        else:
+            log("[Captcha Solver] 進行驗證碼識別...")
+            solved_result = captcha_solver(captcha_image_url, session)
+            captcha_code = handle_captcha_solved_result(solved_result)
+            log("[Captcha Solver] 識別的驗證碼是: {}".format(captcha_code))
+
+            if CHECK_CAPTCHA_SOLVER_USAGE:
+                usage = get_captcha_solver_usage()
+                log(
+                    "[Captcha Solver] current date {0} api usage count: {1}".format(
+                        usage[0]["date"], usage[0]["count"]
+                    )
+                )
+
+            f2 = session.post(
+                url,
+                headers=headers,
+                data={
+                    "subaction": "login",
+                    "sess_id": sess_id,
+                    "captcha_code": captcha_code,
+                },
+            )
+            if (
+                f2.text.find(
+                    "To finish the login process please solve the following captcha."
+                )
+                == -1
+            ):
+                log("[Captcha Solver] 驗證通過")
+                return sess_id, session
+            else:
+                log("[Captcha Solver] 驗證失敗")
+                return "-1", session
+
+    else:
+        return sess_id, session
+
+
+def get_servers(sess_id: str, session: requests.session) -> {}:
+    d = {}
+    url = "https://support.euserv.com/index.iphp?sess_id=" + sess_id
+    headers = {"user-agent": user_agent, "origin": "https://www.euserv.com"}
+    f = session.get(url=url, headers=headers)
+    f.raise_for_status()
+    soup = BeautifulSoup(f.text, "html.parser")
+    for tr in soup.select(
+        "#kc2_order_customer_orders_tab_content_1 .kc2_order_table.kc2_content_table tr"
+    ):
+        server_id = tr.select(".td-z1-sp1-kc")
+        if not len(server_id) == 1:
+            continue
+        flag = (
+            True
+            if tr.select(".td-z1-sp2-kc .kc2_order_action_container")[0]
+            .get_text()
+            .find("Contract extension possible from")
+            == -1
+            else False
+        )
+        d[server_id[0].get_text()] = flag
+    return d
+
+
+def renew(
+    sess_id: str, session: requests.session, password: str, order_id: str
+) -> bool:
+    url = "https://support.euserv.com/index.iphp"
+    headers = {
+        "user-agent": user_agent,
+        "Host": "support.euserv.com",
+        "origin": "https://support.euserv.com",
+        "Referer": "https://support.euserv.com/index.iphp",
+    }
+    data = {
+        "Submit": "Extend contract",
+        "sess_id": sess_id,
+        "ord_no": order_id,
+        "subaction": "choose_order",
+        "choose_order_subaction": "show_contract_details",
+    }
+    session.post(url, headers=headers, data=data)
+    data = {
+        "sess_id": sess_id,
+        "subaction": "kc2_security_password_get_token",
+        "prefix": "kc2_customer_contract_details_extend_contract_",
+        "password": password,
+    }
+    f = session.post(url, headers=headers, data=data)
+    f.raise_for_status()
+    if not json.loads(f.text)["rs"] == "success":
+        return False
+    token = json.loads(f.text)["token"]["value"]
+    data = {
+        "sess_id": sess_id,
+        "ord_id": order_id,
+        "subaction": "kc2_customer_contract_details_extend_contract_term",
+        "token": token,
+    }
+    session.post(url, headers=headers, data=data)
+    time.sleep(5)
+    return True
+
+
+def check(sess_id: str, session: requests.session):
+    print("反饋中.......")
+    d = get_servers(sess_id, session)
+    flag = True
+    for key, val in d.items():
+        if val:
+            flag = False
+            log("[EUserv] ServerID: %s 續期失敗！德雞吐血倒地，請自查原因" % key)
+
+    if flag:
+        log("[EUserv] **********一切OK，德雞在向你微笑！(≧▽≦)**********")
+
+
+# Telegram Bot Push https://core.telegram.org/bots/api#authorizing-your-bot
+def telegram():
+    data = (
+        ('chat_id', TG_USER_ID),
+        ('text', 'EUserv續期日志\n\n' + desp)
+    )
+    response = requests.post('https://' + TG_API_HOST + '/bot' + TG_BOT_TOKEN + '/sendMessage', data=data)
+    if response.status_code != 200:
+        print('Telegram Bot 推送失敗')
+    else:
+        print('Telegram Bot 推送成功')\
+if __name__ == "__main__":
+    if not USERNAME or not PASSWORD:
+        log("[EUserv] 你沒有添加任何賬戶")
+        exit(1)
+    user_list = USERNAME.strip().split()
+    passwd_list = PASSWORD.strip().split()
+    if len(user_list) != len(passwd_list):
+        log("[EUserv] The number of usernames and passwords do not match!")
+        exit(1)
+    for i in range(len(user_list)):
+        print("*" * 30)
+        log("[EUserv] 正在續期第 %d 個賬號" % (i + 1))
+        sessid, s = login(user_list[i], passwd_list[i])
+        if sessid == "-1":
+            log("[EUserv] 第 %d 個賬號登陸失敗，請檢查登錄信息" % (i + 1))
+            continue
+        SERVERS = get_servers(sessid, s)
+        log("[EUserv] 檢測到第 {} 個賬號有 {} 台 VPS，正在嘗試續期".format(i + 1, len(SERVERS)))
+        for k, v in SERVERS.items():
+            if v:
+                if not renew(sessid, s, passwd_list[i], k):
+                    log("[EUserv] ServerID: %s 德雞吐血倒地，請自查原因！" % k)
+                else:
+                    log("[EUserv] ServerID: %s 已到續期時間點，成功續期，下個月見！" % k)
+            else:
+                log("[EUserv] ServerID: %s 未到續期時間點，下回執行見！" % k)
+        time.sleep(15)
+        check(sessid, s)
+        time.sleep(5)
+
+    TG_BOT_TOKEN and TG_USER_ID and TG_API_HOST and telegram()
+    SCKEY and server_chan()
+    SRE24_TOKEN and sre24()
+
+    print("*" * 30)
