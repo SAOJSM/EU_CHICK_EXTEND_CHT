6304d184243a23f6ac03553cbffb2e9ff99550cf
SAOJSM <46414240+SAOJSM@users.noreply.github.com>
2022-04-09 16:41:33 +0800

    Update eu.py
    更新user_agent

--- eu.py
+++ eu.py
@@ -41,8 +41,8 @@ TRUECAPTCHA_CHECK_USAGE = True
 
 
 user_agent = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/94.0.4606.61 Safari/537.36 "
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/99.0.4844.51 Safari/537.36"
 )
 desp = ""  # 空值
 
